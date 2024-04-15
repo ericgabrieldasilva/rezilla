be31eca26ccb2e1d6ec2b05d1650800fef73c7f5
Eric Gabriel <ericgabriel257@gmail.com>
2024-04-15 13:42:40 -0300

    Fixing bugs on mobile menu and edit scrollbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,30 @@
 openMenuMobile()
+headerFixed()
+
 window.onscroll = function () {
   headerFixed()
 }
 
 function openMenuMobile() {
+  const body = document.querySelector("body")
   const menuMobile = document.querySelector(".menu-mobile")
   const btnOpenMenu = document.querySelector("#btn-open-menu")
   const btnCloseMenu = document.querySelector("#btn-close-menu")
 
   btnOpenMenu.addEventListener("click", function (event) {
     menuMobile.classList.add("show-menu-mobile")
+    body.style.overflowY = "hidden"
   })
 
   btnCloseMenu.addEventListener("click", function (event) {
     menuMobile.classList.remove("show-menu-mobile")
+    body.style.overflowY = "visible"
   })
 }
 
 function headerFixed() {
-  const header = document.querySelector("header")
   const adress = document.querySelector(".adress")
+  const header = document.querySelector("header")
 
   if (window.pageYOffset > adress.offsetHeight) {
     header.classList.add("fixed-header")
