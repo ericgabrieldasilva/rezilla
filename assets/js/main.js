ab4e0698e8603564aaed910058388db3354151b1
Eric Gabriel <ericgabriel257@gmail.com>
2024-04-14 21:59:24 -0300

    Add fixed header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,29 @@
-const menuMobile = document.querySelector(".menu-mobile")
-const btnOpenMenu = document.querySelector("#btn-open-menu")
-const btnCloseMenu = document.querySelector("#btn-close-menu")
+openMenuMobile()
+window.onscroll = function () {
+  headerFixed()
+}
 
-btnOpenMenu.addEventListener("click", function(event){
-  menuMobile.classList.add("show-menu-mobile")
-})
+function openMenuMobile() {
+  const menuMobile = document.querySelector(".menu-mobile")
+  const btnOpenMenu = document.querySelector("#btn-open-menu")
+  const btnCloseMenu = document.querySelector("#btn-close-menu")
 
-btnCloseMenu.addEventListener("click", function (event) {
-  menuMobile.classList.remove("show-menu-mobile")
-})
+  btnOpenMenu.addEventListener("click", function (event) {
+    menuMobile.classList.add("show-menu-mobile")
+  })
+
+  btnCloseMenu.addEventListener("click", function (event) {
+    menuMobile.classList.remove("show-menu-mobile")
+  })
+}
+
+function headerFixed() {
+  const header = document.querySelector("header")
+  const adress = document.querySelector(".adress")
+
+  if (window.pageYOffset > adress.offsetHeight) {
+    header.classList.add("fixed-header")
+  } else {
+    header.classList.remove("fixed-header")
+  }
+}
